45bae46dc2bf22edb6dfcdee2d242bd39da5c041
skgchn <sunilkgirdhar@gmail.com>
2020-07-16 11:59:07 +0530

    Update sync_remote.sh
    If the tracking branch already exists, do not do git-branch step, but just display a notice, else git-branch throws a fatal error for the existing branch.

--- sync_remote.sh
+++ sync_remote.sh
@@ -2,12 +2,16 @@
 
 # Idea: https://www.w3docs.com/snippets/git/how-to-fetch-all-git-branches.html
 
-git fetch --prune --all
+git fetch --prune --all 
 
 git branch -r |\
     grep -v '\->' |\
     while read remote; do
-        git branch --track "${remote#origin/}" "$remote";
+        if [[ -z $(git branch --list ${remote#origin/}) ]]; then
+            git branch --track "${remote#origin/}" "$remote"
+        else
+            echo "Notice: Branch named '${remote#origin/}' already exists."
+        fi
     done
 
 git pull --all
